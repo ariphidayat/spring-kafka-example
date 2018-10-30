8cba939cbce5c9a1516de561f1838f31064e05f4
Arip Hidayat <ariphidayat@hotmail.com>
2018-10-30 12:14:14 +0700

    xml config

--- service-three/src/main/java/id/arip/controller/ServiceThreeController.java
+++ service-three/src/main/java/id/arip/controller/ServiceThreeController.java
@@ -3,6 +3,7 @@ package id.arip.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by Arip Hidayat on 18/04/2016.
  */
+@EnableKafka
 @RestController
 public class ServiceThreeController {
     private static final Logger log = LoggerFactory.getLogger(ServiceThreeController.class);
+
+    private static final String TOPIC = "test";
 
     @Autowired
     private KafkaTemplate kafkaTemplate;
@@ -25,11 +29,11 @@ public class ServiceThreeController {
 
     @RequestMapping(value = "/{message}", method = RequestMethod.GET)
     public String sendMessage(@PathVariable("message") String message){
-        kafkaTemplate.send("spring", message);
+        kafkaTemplate.send(TOPIC, message);
         return "You just send a message: " + message;
     }
 
-    @KafkaListener(topics = "spring")
+    @KafkaListener(topics = TOPIC)
     public void subscribe(String message) {
         log.info("Received a message: {}", message);
     }
